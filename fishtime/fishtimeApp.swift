7da1c70f8c23bc845c6825cf76ccae6b5112d916
Pannenets F <pannenets.f@foxmail.com>
2023-04-01 11:55:04 +0800

    Initial Commit

---
+++ fishtime/fishtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fishtimeApp.swift
+//  fishtime
+//
+//  Created by Yunqian Fan on 2023/4/1.
+//
+
+import SwiftUI
+
+@main
+struct fishtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
